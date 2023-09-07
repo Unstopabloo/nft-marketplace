647ddb3c88997c5821fb7180e51aeae3e9d35061
Unstopabloo <pablooyarceramirez@gmail.com>
2023-09-07 13:40:43 -0300

    Se añade modal

---
+++ components/modal.php
@@ -0,0 +1,31 @@
+<section class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog container modal-dialog-centered">
+    <article class="modal-content antonio bg-2 text-1 p-1 p-md-5">
+      <div class="modal-product p-3 p-md-4 container">
+        <div class="row">
+          <img loading="lazy" class="modal-product-img col-md-6 col-12" src="./assets/img/nft_poster_1.webp" alt="Card product image">
+          <div class="modal-product-content col-md-6 col-12">
+            <div class="modal-header row">
+              <span class="modal-artist col-11">Artista destacado</span>
+              <button type="button col-1" class="btn-close text-white" data-bs-dismiss="modal" aria-label="Close"></button>
+              <h2 class="modal-title title-2 col-12" id="exampleModalLabel">Modal title</h2>
+            </div>
+            <div class="modal-body row mt-3">
+              <p class="col-12">
+                Cositas que dira el nft Cositas que dira el nft Cositas que dira el nft Cositas que dira el nft Cositas que dira el nft Cositas que dira el nft
+              </p>
+              <strong class="modal-price font-15 d-flex align-items-center gap-3 pt-2"><span class="badge">0.141 ETH</span> $148 USD</strong>
+            </div>
+            <div class="modal-footer row mt-4">
+              <button type="button" class="btn text-1 border col-4" data-bs-dismiss="modal">Comprar ahora</button>
+              <button type="button" class="btn text-1 border col-4">Hacer oferta</button>
+            </div>
+          </div>
+        </div>
+      </div>
+      
+      
+      
+    </article>
+  </div>
+</section>
