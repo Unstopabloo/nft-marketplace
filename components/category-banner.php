26d0bc9b6896785416fd28eee274f583ddb9865f
Unstopabloo <pablooyarceramirez@gmail.com>
2023-09-10 23:54:30 -0300

    Se añade titulo a banner

--- components/category-banner.php
+++ components/category-banner.php
@@ -1,11 +1,11 @@
 <section class="py-5">
-  <h2 class="text-1 title-2 row py-5">Por colección</h2>
+  <h2 class="text-1 title-2 row py-5">Por categoria</h2>
   <article class="category-banner">
-    <a href="#" style="background-image: url('./assets/img/art1.png');"></a>
-    <a href="#" style="background-image: url('./assets/img/art2.png');"></a>
-    <a href="#" style="background-image: url('./assets/img/art3.png');"></a>
-    <a href="#" style="background-image: url('./assets/img/art4.png');"></a>
-    <a href="#" style="background-image: url('./assets/img/art5.png');"></a>
-    <a href="#" style="background-image: url('./assets/img/PRODUCT-IMAGE.png');"></a>
+    <a href="#" style="background-image: url('./assets/img/art1.png');"><span class="category-banner-title">Categoria</span></a>
+    <a href="#" style="background-image: url('./assets/img/art2.png');"><span class="category-banner-title">Categoria</span></a>
+    <a href="#" style="background-image: url('./assets/img/art3.png');"><span class="category-banner-title">Categoria</span></a>
+    <a href="#" style="background-image: url('./assets/img/art4.png');"><span class="category-banner-title">Categoria</span></a>
+    <a href="#" style="background-image: url('./assets/img/art5.png');"><span class="category-banner-title">Categoria</span></a>
+    <a href="#" style="background-image: url('./assets/img/PRODUCT-IMAGE.png');"><span class="category-banner-title">Categoria</span></a>
   </article>
 </section>
