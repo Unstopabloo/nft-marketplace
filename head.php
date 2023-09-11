56c0c937a272b07f8078eeca8f5276308554a574
samxever <sa.casaran@duocuc.cl>
2023-09-11 19:58:28 -0300

    cambio de titulo

--- head.php
+++ head.php
@@ -3,7 +3,7 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>NFT Marketplace 😎</title>
+    <title> Marketplace 😎</title>
     <link rel="shortcut icon" href="./assets/img/favicon.png" type="png/x-icon">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
     <link rel="stylesheet" href="./assets/css/splide.min.css">
